77431fcdf0636a7bf7eb8fc12de0bb33d77b9313
Radosław Kobyłecki <radoslaw.kobylecki2@teb.edu.pl>
2023-09-06 12:35:41 +0200

    Catch the button game

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -9,7 +9,27 @@ namespace WinFormsApp1
 
         private void BtnClick(object sender, EventArgs e)
         {
-            Application.Exit();
+            //Application.Exit();
+
+            //szerokosc okna
+            int windowWidth = Size.Width;
+            //wysokosc okna
+            int windowHeigh = Size.Height;
+            //Unikamy sytuacji, ze guzik ucieka z ekranu
+            windowWidth -= button1.Width;
+            windowHeigh -= button1.Height;
+
+            //inicjalizacja generatora liczb losowych
+            Random rnd = new Random();
+            //losowa odleglosc od lewej
+            int randomLeft = rnd.Next(windowWidth);
+            //losowa odleglosc od gory
+            int randomTop = rnd.Next(windowHeigh);
+            //przesuwany guzik
+            //location musi byc podana jako punkt na ekranie
+            Point topLeftCorner = new Point(randomLeft, randomTop);
+            //button1 -> atrybut mojego przycisku :>
+            button1.Location = topLeftCorner;
         }
     }
 }
