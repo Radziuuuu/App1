a6435b66abff4bd356a54eb869c27434658d7a49
Radosław Kobyłecki <radoslaw.kobylecki2@teb.edu.pl>
2023-09-06 12:10:33 +0200

    Exit button

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
